05eb66cf61142f6ffc8b8c45a79be0de9d837659
Andreas Franz <andreas.franz@ecily.com>
2024-01-03 08:47:50 +0100

    create test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
+import userRouter from "./routes/user.route.js"
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected to mongo")
@@ -13,3 +14,13 @@ const app = express()
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+//als beispiel, in der praxis werden routes und die funktionen in eigenen folder und files gelegt
+//app.get("/test", (req, res) => {
+    //res.send("Hello ecily")
+    //res.json({
+       // message: "hello world"
+    //})
+//})
+
+app.use("/api/user", userRouter)
